0b3d5f043948b77ff0d156823016b2088356aa07
Bruno Paranhos Silva <62160761+paranhossilva@users.noreply.github.com>
2021-10-19 09:01:24 -0300

    Add files via upload

---
+++ TP02/DAO/CategoriasDAO.cs
@@ -0,0 +1,100 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using TP02.Models;
+
+namespace TP02.DAO
+{
+    public class CategoriasDAO
+    {
+        private MySqlConnection conexao;
+        private MySqlCommand comando;
+        private MySqlDataReader dr;
+
+        private void open()
+        {
+            conexao = new MySqlConnection(ConfigurationManager.ConnectionStrings["Connection"].ConnectionString);
+            conexao.Open();
+        }
+
+        private void close() { conexao.Close(); }
+
+        public void adiciona(Categoria categ)
+        {
+            open();
+
+            String sql = $"insert into categoria (nome, descricao) values ('{categ.Nome}', '{categ.Desc}');";
+
+            comando = new MySqlCommand(sql, conexao);
+            comando.ExecuteNonQuery();
+
+            close();
+        }
+
+        public IList<Categoria> lista()
+        {
+            open();
+
+            IList<Categoria> categs = new List<Categoria>();
+
+            String sql = "select * from categoria;";
+
+            comando = new MySqlCommand(sql, conexao);
+            dr = comando.ExecuteReader();
+
+            while (dr.Read())
+            {
+                categs.Add(new Categoria(dr.GetInt32(0), dr.GetString(1), dr.GetString(2)));
+            }
+
+            close();
+
+            return categs;
+        }
+
+        public Categoria buscaID(int id)
+        {
+            open();
+
+            String sql = $"select * from categoria where id = {id};";
+
+            comando = new MySqlCommand(sql, conexao);
+            dr = comando.ExecuteReader();
+
+            dr.Read();
+
+            Categoria categ = new Categoria(dr.GetInt32(0), dr.GetString(1), dr.GetString(2));
+
+            close();
+
+            return categ;
+        }
+
+        public void atualiza(Categoria categ)
+        {
+            open();
+
+            String sql = $"update categoria set nome = {categ.Nome}, descricao = {categ.Desc};";
+
+            comando = new MySqlCommand(sql, conexao);
+            comando.ExecuteNonQuery();
+
+            close();
+        }
+
+        public void exclui(int id)
+        {
+            open();
+
+            String sql = $"delete from categoria where id = {id};";
+
+            comando = new MySqlCommand(sql, conexao);
+            comando.ExecuteNonQuery();
+
+            close();
+        }
+    }
+}
